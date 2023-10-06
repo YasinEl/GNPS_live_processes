8c151158266cd404a483be2f2a111119a2db1693
Mingxun Wang <mwang87@gmail.com>
2023-10-05 17:37:19 -0700

    updating output

--- dash/plotlyDash.py
+++ dash/plotlyDash.py
@@ -236,7 +236,18 @@ def prepare_pca_data(df, intensity_column = 'all MZbins'):
 
 
 # Initialize app
-dash_app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.FLATLY])
+try:
+    # This is a part of a flask app
+    from app import app
+    dash_app = dash.Dash(   __name__, 
+                        suppress_callback_exceptions=True, 
+                        external_stylesheets=[dbc.themes.FLATLY], 
+                        server=app,
+                        url_base_pathname="/dashinterface1/")
+
+except:
+    # This is standalone
+    dash_app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.FLATLY])
 
 
 #load data for everything
